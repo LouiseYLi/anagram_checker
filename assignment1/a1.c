1b9628752a79f3e134c02e92b3662621a599708f
Louise <louiseli.van@gmail.com>
2024-02-09 09:00:00 -0800

    updated overall code

--- assignment1/a1.c
+++ assignment1/a1.c
@@ -1,26 +1,88 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// For testing
-void printArray(char arr[], int arrSize) {
-    for (int i = 0; i < arrSize; i++) {
-        printf("")
+
+
+int isAnagram(FILE *input, FILE *output) {
+    int charOfWord[26] = {0};
+    int charOfAnagram[26] = {0};
+
+
+
+    int index = 0;
+    char inputChar;
+    while (inputChar != '\n') {
+        inputChar = fgetc(input);
+
+        index++;
     }
-}
+    char *firstString = malloc(index * sizeof(char));
+    
+    fseek(input, 0, SEEK_SET);
 
-void reverseArray(char arr[], int arrSize) {
-    char temp;
-    for (int i = 0; i < arrSize; i++) {
-        for (int j = i + 1; j < arrSize; j++) {
-            temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+    fgets(firstString, index, input);
+
+    int index2 = index + 1;
+    while (inputChar != EOF) {
+        inputChar = fgetc(input);
+
+        index2++;
+    }
+
+    fseek(input, index, SEEK_SET);
+
+    index2 -= index;
+    char *secondString = malloc((index2) * sizeof(char));
+
+    fgets(secondString, index2, input);
+
+    printf("first: %s second: %s\n", firstString,secondString);
+
+    for (int i = 0; i < index; i++) {
+        printf("%c\n", firstString[i]);
+        if (firstString[i] >= 'a' && firstString[i] <= 'z') {
+            charOfWord[firstString[i] - 'a']++;
+        }
+        else if (firstString[i] >= 'A' && firstString[i] <= 'Z') {
+            charOfWord[firstString[i] - 'A']++;
         }
     }
-    
+
+    for (int i = 0; i < index; i++) {
+        if (secondString[i] >= 'a' && secondString[i] <= 'z') {
+            charOfAnagram[secondString[i] - 'a']++;
+        }
+        else if (secondString[i] >= 'A' && secondString[i] <= 'Z') {
+            charOfAnagram[secondString[i] - 'A']++;
+        }
+    }
+
+    free(firstString);
+    free(secondString);
+
+    for (int i = 0; i < 26; i++) {
+        printf("first: %d second: %d\n", charOfWord[i], charOfAnagram[i]);
+        if(charOfWord[i] != charOfAnagram[i]) {
+            fprintf(output, "0! not anagram");
+            return 0;
+        }
+    }
+
+    fprintf(output, "1! anagram");
+    return 1;
 }
 
-int main() {
-    char c[] = "cool";
-    reverseArray(c, 4);
-    return 0;
+int main(int argc, char **argv){
+    char *inputFile = argv[1];
+    char *outputFile = argv[2];
+
+    FILE *input = fopen(inputFile, "r");
+
+    FILE *output = fopen(outputFile, "w");
+
+    isAnagram(input, output);
+
+    fclose(input);
+    fclose(output);
 }
